ef6239d19bed866f28a437abc06f1456c6bb2145
ArtyomOsipchuk <artos4002@yandex.ru>
2025-07-07 14:55:00 +0300

    Login via local and then ldap users - Autologin OFF - Bug with sshd resolved

--- features.in/04cmcldap/live/image-scripts.d/99-common-auth.sh
+++
@@ -1,4 +0,0 @@
-echo """
-auth sufficient pam_unix.so try_first_pass nullok
-account sufficient pam_unix.so
-""" >> /etc/pam.d/system-auth-common
--- features.in/04cmcldap/live/image-scripts.d/99-pam-local+ldap_login.sh
+++
@@ -1,5 +0,0 @@
-sed -i 's/^account sufficient pam_unix.so//' /etc/pam.d/system-auth-common
-echo """
-auth sufficient pam_ldap.so use_first_pass
-auth required pam_deny.so
-""" >> /etc/pam.d/system-auth-common
