7fe0b4bb7d14c56bc7f6e0e6b04406dc98ebaa31
Your Name <you@example.com>
2025-07-21 22:02:10 +0300

    Fixed localdomain fetch

--- features.in/03unroot/live/files/usr/local/bin/replace-localdomain.sh
+++ features.in/03unroot/live/files/usr/local/bin/replace-localdomain.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IP=$(hostname -I | awk '{print $1}')
+IP=$(ip -o a show scope global | awk '{print $4}' | cut -d'/' -f1 | head -n1)
 NEW_DOMAIN=$(dig -x "$IP" +short | sed 's/\.$//')
 
 [ -z "$NEW_DOMAIN" ] && NEW_DOMAIN="localdomain"
