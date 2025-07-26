dd1a2c893f06fd4ce7e36129018921ddd41bfc81
Artyom Osipchuk <artos4002@yandex.ru>
2025-07-26 12:33:40 +0300

    Added condition not to restart nfs service - FIXED bug with ~/.local/bin in $PATH

--- features.in/03unroot/live/files/etc/bashrc.d/local.sh
+++ features.in/03unroot/live/files/etc/bashrc.d/local.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo """
 case "$PATH" in
 	*:$HOME/bin|*:$HOME/bin:*|$HOME/bin:*) ;;
 	*) PATH="$HOME/bin:$PATH" ;;
@@ -9,4 +8,3 @@ case "$PATH" in
 	*:$HOME/.local/bin|*:$HOME/.local/bin:*|$HOME/.local/bin:*) ;;
 	*) PATH="$HOME/.local/bin:$PATH" ;;
 esac
-""" >> /etc/skel/.bashrc
